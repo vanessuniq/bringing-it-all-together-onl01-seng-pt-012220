03990d273bc2f015929206685228c15305cdc387
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-10 05:21:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,8 +27,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    dog = self.new(row[0], row[1], row[2])
-    dog
+    attributes = {id: row[0], name: row[1], breed: row[2]}
   end
   
   def self.find_by_name(name)
