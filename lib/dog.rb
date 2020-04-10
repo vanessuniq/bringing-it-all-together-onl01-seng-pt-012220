c7a0d6cbb4cad424fa21433e883269b26997204f
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-10 05:08:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :id, :name, :breed
   
   def initialize (attributes)
-    attributes.each {|k, v| send(("#{k}="), value)}
+    attributes.each {|k, v| send(("#{k}="), v)}
     @id ||= nil
   end
   
